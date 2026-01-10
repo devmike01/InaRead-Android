3cb33305e8f787a156153f3dbea1ab3046084f02
Devmike01 <michaelgbenga91@gmail.com>
2026-01-10 17:42:42 +0100

    added string tokens

--- app/src/main/java/dev/gbenga/inaread/tokens/StringTokens.kt
+++ app/src/main/java/dev/gbenga/inaread/tokens/StringTokens.kt
@@ -1,4 +1,9 @@
 package dev.gbenga.inaread.tokens
 
 object StringTokens {
+
+    const val DailyActivity = "Daily Activity"
+    const val Hours = "Last Usage"
+    const val Days = "Days"
+    const val Cost = "Cost"
 }
