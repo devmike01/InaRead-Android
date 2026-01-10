7bb9af1169e851df72a277ddefe6b5ead8e5879d
Devmike01 <michaelgbenga91@gmail.com>
2026-01-10 17:41:41 +0100

    added dimension tokens

--- app/src/main/java/dev/gbenga/inaread/tokens/DimenTokens.kt
+++ app/src/main/java/dev/gbenga/inaread/tokens/DimenTokens.kt
@@ -1,21 +1,42 @@
 package dev.gbenga.inaread.tokens
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 object DimenTokens {
 
     object FontSize {
-        val H1 = 30.dp
+        val h1 = 30.sp
+        val h2 = 20.sp
     }
 
     object Padding {
-        val small = 10.dp
+        val xSmall = 5.dp
+        val small = xSmall * 2
+        val normal = small * 2
+        val large = normal * 2
+    }
+
+    object HomeSummaryCard{
+        val innerPadding = 20.dp
     }
 
     object Radius{
         val xSmall = 5.dp
         val small = xSmall * 2
         val large = small * 2
+    }
+
+    object Size {
+        val topbar = 100.dp
+    }
+
+    object CalendarItem{
+
+        val height = 80.dp
+        val width = 70.dp
+        val padding = 15.dp
+        val outerPadding = 5.dp
     }
 
 }
