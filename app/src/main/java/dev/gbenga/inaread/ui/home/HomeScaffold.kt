f265afe824f48dad59826676e915cb20399d2a12
Devmike01 <michaelgbenga91@gmail.com>
2026-01-10 17:44:37 +0100

    removed useless codes from activity

--- app/src/main/java/dev/gbenga/inaread/ui/home/HomeScaffold.kt
+++ app/src/main/java/dev/gbenga/inaread/ui/home/HomeScaffold.kt
@@ -2,10 +2,15 @@ package dev.gbenga.inaread.ui.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronLeft
@@ -14,13 +19,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.gbenga.inaread.tokens.DimenTokens
+import dev.gbenga.inaread.tokens.StringTokens
 import dev.gbenga.inaread.ui.customs.NullableText
+import dev.gbenga.inaread.ui.customs.VerticalCenter
 
 
 data class HomeScaffoldConfig(
@@ -35,20 +46,42 @@ fun HomeScaffold(inaScaffoldConfig: HomeScaffoldConfig, content: @Composable (Pa
     Scaffold(
         topBar = {
             TopAppBar(
+                modifier = Modifier.height(DimenTokens.Size.topbar),
                 title = {
-                    Column {
+                    Column(
+                        modifier = Modifier.fillMaxHeight(),
+                        verticalArrangement = Arrangement.Center) {
                         inaScaffoldConfig.title.NullableText(
-                            style = MaterialTheme.typography.bodySmall
+                            style = MaterialTheme.typography.bodyMedium
                         )
+                        Text(StringTokens.DailyActivity,
+                            style = MaterialTheme.typography
+                                .headlineLarge.copy(fontWeight = FontWeight.W800),
+                            )
                     }
                 },
                 actions = {
-
+                    InitialComponent("G")
                 }
             )
         }
     ) {
         content(it)
+    }
+}
+
+
+@Composable
+fun InitialComponent(initial: String){
+    VerticalCenter(modifier = Modifier.fillMaxHeight()) {
+        Box(modifier = Modifier.size(80.dp)
+            .padding(DimenTokens.Padding.small)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.primaryContainer)
+        ) {
+            initial.NullableText(modifier = Modifier.align(Alignment.Center),
+                style = MaterialTheme.typography.headlineMedium)
+        }
     }
 }
 
