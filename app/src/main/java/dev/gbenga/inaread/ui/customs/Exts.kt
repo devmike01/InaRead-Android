98eb9e889870aa455aa586f3e5dd4d4b4c1b0673
Devmike01 <michaelgbenga91@gmail.com>
2026-01-10 17:43:06 +0100

    added extentions

--- app/src/main/java/dev/gbenga/inaread/ui/customs/Exts.kt
+++ app/src/main/java/dev/gbenga/inaread/ui/customs/Exts.kt
@@ -5,14 +5,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 
 
 @Composable
-fun String?.NullableText(style: TextStyle= LocalTextStyle.current){
+fun String?.NullableText(modifier: Modifier = Modifier, style: TextStyle= LocalTextStyle.current){
     this?.let {
-        Text(it, style = style)
+        Text(it, style = style, modifier = modifier)
     }
 }
 
@@ -28,3 +30,5 @@ fun IconConfig?.NullableIcon(){
 
 
 data class IconConfig(@param:DrawableRes val iconResId: Int, val description: String?)
+
+fun Long.color(): Color = Color(this)
