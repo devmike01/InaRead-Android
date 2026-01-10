9fa41e4da3143b4469ad329b1aa97eac085b9223
Devmike01 <michaelgbenga91@gmail.com>
2026-01-10 17:49:10 +0100

    updated home screen

--- app/src/main/java/dev/gbenga/inaread/ui/home/HomeScreen.kt
+++ app/src/main/java/dev/gbenga/inaread/ui/home/HomeScreen.kt
@@ -2,11 +2,17 @@ package dev.gbenga.inaread.ui.home
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import dev.gbenga.inaread.tokens.StringTokens
 
 @Composable
 fun HomeScreen() {
 
-    Scaffold() { }
+    HomeScaffold(HomeScaffoldConfig(
+        title = StringTokens.DailyActivity,
+        navigationClick = {}
+    )){
+
+    }
 
 
 }
