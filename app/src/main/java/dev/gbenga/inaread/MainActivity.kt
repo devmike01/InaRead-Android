c7b89664a15bf3d13f586b95857d4db469c924da
Devmike01 <michaelgbenga91@gmail.com>
2026-01-12 16:14:53 +0100

    removed useless codes

--- app/src/main/java/dev/gbenga/inaread/MainActivity.kt
+++ app/src/main/java/dev/gbenga/inaread/MainActivity.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import dev.gbenga.inaread.ui.theme.InaReadTheme
 import dev.gbenga.inaread.utils.nav.InaReadApp
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            InaReadTheme {
-                InaReadApp()
-            }
+           setContent {
+               InaReadApp()
         }
     }
 }
