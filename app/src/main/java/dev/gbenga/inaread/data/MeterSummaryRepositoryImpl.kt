954be681e4df9ff11568d474482916746993fb67
Devmike01 <michaelgbenga91@gmail.com>
2026-01-13 18:20:21 +0100

    refactored meter summary

--- app/src/main/java/dev/gbenga/inaread/data/MeterSummaryRepositoryImpl.kt
+++ app/src/main/java/dev/gbenga/inaread/data/MeterSummaryRepositoryImpl.kt
@@ -1,16 +1,22 @@
 package dev.gbenga.inaread.data
 
 import dev.gbenga.inaread.data.model.MeterResponse
-import dev.gbenga.inaread.data.service.MeterSummaryApiService
-import dev.gbenga.inaread.domain.usescases.meter.MeterSummaryRepository
+import dev.gbenga.inaread.domain.datastore.UserDataStore
+import dev.gbenga.inaread.domain.meter.MeterSummaryRepository
+import dev.gbenga.inaread.domain.services.MeterSummaryApiService
+import dev.gbenga.inaread.utils.UserNotLoggedInException
+import kotlinx.coroutines.flow.firstOrNull
 
 class MeterSummaryRepositoryImpl (
     private val meterSummaryApiService: MeterSummaryApiService,
-
+    private val userDataStore: UserDataStore,
 ) : MeterSummaryRepository {
 
     override suspend fun getMeterSummary(): MeterResponse {
-        return meterSummaryApiService.fetchMeterSummary()
+        val profileId = userDataStore.getProfileId().firstOrNull()
+        return profileId?.let {
+            meterSummaryApiService.fetchMeterSummary(profileId)
+        } ?: throw UserNotLoggedInException()
     }
 
 
