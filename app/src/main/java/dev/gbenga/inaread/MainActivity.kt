95ecb0b01c43e256ebc18465d4a606e4c64a492f
Devmike01 <michaelgbenga91@gmail.com>
2026-01-14 10:29:33 +0100

    Refactored main activity

--- app/src/main/java/dev/gbenga/inaread/MainActivity.kt
+++ app/src/main/java/dev/gbenga/inaread/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
+import dev.gbenga.inaread.ui.addreading.ImagePickerProvider
 import dev.gbenga.inaread.ui.theme.InaReadTheme
 import dev.gbenga.inaread.utils.nav.InaReadApp
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
            setContent {
                InaReadApp()
