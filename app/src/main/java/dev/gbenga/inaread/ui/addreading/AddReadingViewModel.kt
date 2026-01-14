d8da29dd0d10a80463e87aadeb0960d544f2f6c6
Devmike01 <michaelgbenga91@gmail.com>
2026-01-14 10:34:01 +0100

    added viewmodel for add new reading record

---
+++ app/src/main/java/dev/gbenga/inaread/ui/addreading/AddReadingViewModel.kt
@@ -0,0 +1,29 @@
+package dev.gbenga.inaread.ui.addreading
+
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.gbenga.inaread.utils.InaReadViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddReadingViewModel @Inject constructor() : InaReadViewModel<AddReadingState, AddReadingEvent>(AddReadingState()) {
+
+    override fun watchEvents() {
+        viewModelScope.launch {
+            events.collect { events ->
+                when(events){
+                    is AddReadingEvent.GetMeterImageResult -> {
+                        setState { it.copy( meterImagePath = events.imagePath) }
+                    }
+                }
+            }
+        }
+    }
+
+    fun addImage(path: String?){
+        path?.let {
+            sendEvent(AddReadingEvent.GetMeterImageResult(path))
+        }
+    }
+}
