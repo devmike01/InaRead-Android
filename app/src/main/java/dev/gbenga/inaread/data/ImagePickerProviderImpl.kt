8b9350f219cd377c29511792ea9ffbc1b485e371
Devmike01 <michaelgbenga91@gmail.com>
2026-01-15 14:17:38 +0100

    Added image picker

--- app/src/main/java/dev/gbenga/inaread/data/ImagePickerProviderImpl.kt
+++ app/src/main/java/dev/gbenga/inaread/data/ImagePickerProviderImpl.kt
@@ -3,15 +3,20 @@ package dev.gbenga.inaread.data
 import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.gbenga.inaread.domain.date.CalendarProvider
 import dev.gbenga.inaread.domain.providers.ImagePickerProvider
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.withContext
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
 import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
@@ -20,10 +25,34 @@ class ImagePickerProviderImpl @Inject constructor(
     private val ioDispatcher : CoroutineContext,
 ): ImagePickerProvider {
 
+    companion object{
+        val RESIZED_IMG_SIZE = 700
+    }
+
     private val contentResolver: ContentResolver get() = context.contentResolver
+
+    fun InputStream.resizeBitmap(uri: Uri){
+        val bitmap = BitmapFactory.decodeStream(this)
+
+        val options = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+        }
+        BitmapFactory.decodeStream(this, null, options)
+        options.inSampleSize = calculateInSampleSize(options, RESIZED_IMG_SIZE, RESIZED_IMG_SIZE)
+        options.inJustDecodeBounds = false
+
+        val scaledInputStream = contentResolver.openInputStream(uri)
+        BitmapFactory.decodeStream(scaledInputStream, null, options)
+
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 60, outputStream)
+    }
 
     override fun uriToBytes(uri: Uri): ByteArray? {
         val inputStream = contentResolver.openInputStream(uri)
+//        val bitmap = BitmapFactory.decodeStream(inputStream)
+//        val outputStream = ByteArrayOutputStream()
+//        bitmap.compress(Bitmap.CompressFormat.JPEG, 60, outputStream)
         return inputStream.use {
             uri.grantReadPermissionUri()
             inputStream?.readBytes()
@@ -40,6 +69,7 @@ class ImagePickerProviderImpl @Inject constructor(
             }
             val imageBytes = uriToBytes(uri = uri)
             val fos = FileOutputStream(meterImageFile.path)
+
             try{
                 fos.write(imageBytes)
                 completable.complete(meterImageFile.absolutePath)
@@ -50,6 +80,20 @@ class ImagePickerProviderImpl @Inject constructor(
             }
         }
         return completable.await()
+    }
+
+    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+        val (height: Int, width: Int) = options.run { outHeight to outWidth }
+        var inSampleSize = 1
+
+        if (height > reqHeight || width > reqWidth) {
+            val halfHeight: Int = height / 2
+            val halfWidth: Int = width / 2
+            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
+                inSampleSize *= 2
+            }
+        }
+        return inSampleSize
     }
 
     private fun Uri.grantReadPermissionUri(){
