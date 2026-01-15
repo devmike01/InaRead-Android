a1d69e437b24f2e2d58e5b161125b98d5604133a
Devmike01 <michaelgbenga91@gmail.com>
2026-01-15 14:18:35 +0100

    added more events

--- app/src/main/java/dev/gbenga/inaread/ui/addreading/AddReadingEvent.kt
+++ app/src/main/java/dev/gbenga/inaread/ui/addreading/AddReadingEvent.kt
@@ -5,4 +5,6 @@ import android.net.Uri
 sealed interface AddReadingEvent {
 
     data class GetMeterImageResult(val imageUri: Uri): AddReadingEvent
+    data class ToggleMeterImageButton(val enabled: Boolean): AddReadingEvent
+    data object RemoveImage: AddReadingEvent
 }
