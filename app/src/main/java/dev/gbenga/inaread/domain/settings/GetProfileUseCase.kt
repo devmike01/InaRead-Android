caba65d8e3b13a46ba931be6cade4e20d913acae
Devmike01 <michaelgbenga91@gmail.com>
2026-01-17 19:39:07 +0100

    Added get profile use case

--- app/src/main/java/dev/gbenga/inaread/domain/settings/GetProfileUseCase.kt
+++ app/src/main/java/dev/gbenga/inaread/domain/settings/GetProfileUseCase.kt
@@ -1,7 +1,6 @@
 package dev.gbenga.inaread.domain.settings
 
 import dev.gbenga.inaread.data.model.Profile
-import dev.gbenga.inaread.data.model.ProfileResponse
 import javax.inject.Inject
 
 class GetProfileUseCase @Inject constructor(private val settingsRepository: SettingsRepository) {
