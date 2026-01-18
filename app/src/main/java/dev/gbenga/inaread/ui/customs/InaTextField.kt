d14194a4caf16bc764ed332822932c5148c90565
Devmike01 <michaelgbenga91@gmail.com>
2026-01-18 12:12:57 +0100

    updated ina textfield

--- app/src/main/java/dev/gbenga/inaread/ui/customs/InaTextField.kt
+++ app/src/main/java/dev/gbenga/inaread/ui/customs/InaTextField.kt
@@ -1,11 +1,21 @@
 package dev.gbenga.inaread.ui.customs
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import dev.gbenga.inaread.tokens.DimenTokens
+import dev.gbenga.inaread.ui.theme.Indigo600
 
 
 data class InaTextFieldConfig(
@@ -40,4 +50,31 @@ fun InaTextField(value: String,
             config.leadingIcon.NullableIcon()
         },
         )
+}
+
+
+
+@Composable
+fun InaSingleTextField(
+    modifier: Modifier =Modifier,
+    value: String,placeholder: String,
+    onValueChange: (String) -> Unit,){
+    TextField(value,
+        modifier = modifier
+            .height(DimenTokens.Auth.TextFieldHeight)
+            .padding(vertical = DimenTokens.Padding.small)
+            .fillMaxWidth(),
+        onValueChange = onValueChange,
+        singleLine = true,
+        placeholder = {
+            Text(placeholder)
+        },
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            unfocusedContainerColor = Indigo600,
+            focusedContainerColor = MaterialTheme.colorScheme.secondary
+        ),
+        shape = RoundedCornerShape(DimenTokens.Radius.large)
+    )
 }
