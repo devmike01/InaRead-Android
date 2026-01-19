529a2c333cf2b098a608f77ae43724932ba9983b
Devmike01 <michaelgbenga91@gmail.com>
2026-01-19 19:59:25 +0100

    added base model for api response

--- app/src/main/java/dev/gbenga/inaread/data/model/ApiResult.kt
+++ app/src/main/java/dev/gbenga/inaread/data/model/ApiResult.kt
@@ -1,4 +1,8 @@
 package dev.gbenga.inaread.data.model
 
-data class ApiResult<T>(val data: T,
-                        val error: String)
+data class ApiResult<T>(val data: T? = null,
+                        val error: String? = null){
+
+    val isSuccess = data != null
+    val isError = error != null
+}
