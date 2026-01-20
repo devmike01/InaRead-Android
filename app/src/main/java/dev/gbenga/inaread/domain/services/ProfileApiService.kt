af79ae57cbaa228b7cb34f8f17a04ab3cb621204
Devmike01 <michaelgbenga91@gmail.com>
2026-01-20 16:14:45 +0100

    updated profile service

--- app/src/main/java/dev/gbenga/inaread/domain/services/ProfileApiService.kt
+++ app/src/main/java/dev/gbenga/inaread/domain/services/ProfileApiService.kt
@@ -1,9 +1,11 @@
 package dev.gbenga.inaread.domain.services
 
 import dev.gbenga.inaread.data.model.ProfileResponse
+import retrofit2.http.GET
 
 interface ProfileApiService {
 
+    @GET("/profile")
     suspend fun getProfile(): ProfileResponse
 
 }
