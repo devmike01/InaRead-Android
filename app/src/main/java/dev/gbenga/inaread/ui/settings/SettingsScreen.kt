a86d409d9462661d00ab4fac0f45a35c7aa79f4c
Devmike01 <michaelgbenga91@gmail.com>
2026-01-20 16:18:17 +0100

    reformat codes

--- app/src/main/java/dev/gbenga/inaread/ui/settings/SettingsScreen.kt
+++ app/src/main/java/dev/gbenga/inaread/ui/settings/SettingsScreen.kt
@@ -51,25 +51,31 @@ fun SettingsScreen(settingsViewModel: SettingsViewModel = hiltViewModel()) {
 
     val uiState by settingsViewModel.state.collectAsStateWithLifecycle()
 
-     val onItemClick = remember {
+    val onItemClick = remember {
         { value: String -> /* do something here */ }
     }
 
 
-    Column(modifier = Modifier.padding(DimenTokens.Padding.Small).fillMaxSize()) {
+    Column(modifier = Modifier
+        .padding(DimenTokens.Padding.Small)
+        .fillMaxSize()) {
 
         UnitLaunchEffect {
             settingsViewModel.populateSettings()
         }
 
-        TitledColumn("Your Profile and Settings",
-            "Change from light to dark mode. Edit your profile and many more.",) {
-            LazyColumn(modifier = Modifier
-                .padding(vertical = DimenTokens.Padding.Normal)
-                .fillMaxSize(),
+        TitledColumn(
+            "Your Profile and Settings",
+            "Change from light to dark mode. Edit your profile and many more.",
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(vertical = DimenTokens.Padding.Normal)
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement
-                    .spacedBy(DimenTokens.Padding.Normal)) {
+                    .spacedBy(DimenTokens.Padding.Normal)
+            ) {
 
                 item {
                     ProfileSummaryContent(settingsViewModel)
@@ -85,40 +91,50 @@ fun SettingsScreen(settingsViewModel: SettingsViewModel = hiltViewModel()) {
 
 
 @Composable
-fun LazyItemScope.SettingsContent(settingsViewModel: SettingsViewModel,
-                                  onItemClick: (String) -> Unit){
+fun LazyItemScope.SettingsContent(
+    settingsViewModel: SettingsViewModel,
+    onItemClick: (String) -> Unit
+) {
     val settingsMenu by settingsViewModel.menuItems.collectAsStateWithLifecycle(
         initialValue = emptyList()
     )
 
     AllTimeTitle("Settings")
-    SettingsMenu(Modifier.animateItem(),
+    SettingsMenu(
+        Modifier.animateItem(),
         settingsMenu,
-        onItemClick)
+        onItemClick
+    )
 }
 
 @Composable
-fun LazyItemScope.ProfileSummaryContent(settingsViewModel: SettingsViewModel){
+fun LazyItemScope.ProfileSummaryContent(settingsViewModel: SettingsViewModel) {
     val settingsUiState by settingsViewModel.state.collectAsStateWithLifecycle()
 
     AllTimeTitle("Profile")
 
-    UiStateWithLoadingBox(settingsUiState.profile,
+    UiStateWithLoadingBox(
+        settingsUiState.profile,
         errorRequest = {
-        Scada.info("error---->: $it")
-            InaCard(modifier = Modifier
-                .wrapContentHeight()
-                .height(DimenTokens.Size.topbar)) {
-                Box(modifier = Modifier.fillMaxSize()){
+            Scada.info("error---->: $it")
+            InaCard(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .height(DimenTokens.Size.topbar)
+            ) {
+                Box(modifier = Modifier.fillMaxSize()) {
                     Text(it, modifier = Modifier.align(Alignment.Center))
                 }
             }
-    }) { profile ->
-        ProfileSummary(Modifier
-            .wrapContentHeight()
-            .fillMaxWidth().animateItem(), profile.initial,
+        }) { profile ->
+        ProfileSummary(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .animateItem(), profile.initial,
             profile.username,
-            profile.email)
+            profile.email
+        )
     }
 
 
@@ -126,10 +142,12 @@ fun LazyItemScope.ProfileSummaryContent(settingsViewModel: SettingsViewModel){
 
 
 @Composable
-fun TitledColumn(title: String,
-                     subTitle: String? =null,
-                     modifier: Modifier =Modifier,
-                     content: @Composable () -> Unit) {
+fun TitledColumn(
+    modifier: Modifier = Modifier,
+    title: String,
+    subTitle: String? = null,
+    content: @Composable () -> Unit
+) {
     HorizontalCenter(
         modifier = modifier
             .padding(DimenTokens.Padding.Normal)
@@ -138,52 +156,73 @@ fun TitledColumn(title: String,
             Pair(it.fontSize, it.fontWeight)
         }
         val titleSubTitle = buildAnnotatedString {
-            withStyle(style = SpanStyle(
-                fontSize = titleFont,
-                fontWeight = titleWeight,
-                color = White,
-            )){
+            withStyle(
+                style = SpanStyle(
+                    fontSize = titleFont,
+                    fontWeight = titleWeight,
+                    color = White,
+                )
+            ) {
                 append("$title\n\n")
             }
             subTitle?.let {
                 append(subTitle)
             }
         }
-        Text(titleSubTitle, style = MaterialTheme.typography
-            .bodyMedium.copy(fontWeight = FontWeight.W400,
-                color = Indigo50
-            ),
-            textAlign = TextAlign.Center)
+        Text(
+            titleSubTitle, style = MaterialTheme.typography
+                .bodyMedium.copy(
+                    fontWeight = FontWeight.W400,
+                    color = Indigo50
+                ),
+            textAlign = TextAlign.Center
+        )
         content()
     }
 }
+
 @Composable
-fun SettingsMenu(modifier: Modifier,items: List<VectorInaTextIcon>,
-                 onItemClick: (String) -> Unit){
-    InaCard(modifier = modifier.fillMaxWidth()
-        .wrapContentHeight()) {
-        items.forEachIndexed { index,  item ->
+fun SettingsMenu(
+    modifier: Modifier, items: List<VectorInaTextIcon>,
+    onItemClick: (String) -> Unit
+) {
+    InaCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        items.forEachIndexed { index, item ->
             Column {
-                Row(modifier = Modifier.fillMaxSize().clickable{
-                    onItemClick.invoke(item.value)
-                },
-                    verticalAlignment = Alignment.CenterVertically) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable {
+                            onItemClick.invoke(item.value)
+                        },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     item.icon?.let { icon ->
-                        Icon(imageVector = icon,
+                        Icon(
+                            imageVector = icon,
                             contentDescription = item.label,
                             modifier = Modifier.size(DimenTokens.Icon.Small),
                             tint = Color(item.color)
                         )
                     }
-                    Text(item.label,
-                        style = MaterialTheme.typography.bodyMedium,modifier = Modifier
-                            .padding(horizontal = DimenTokens.Padding.Normal,
-                                vertical = DimenTokens.Padding.Normal)
-                        ,)
+                    Text(
+                        item.label,
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .padding(
+                                horizontal = DimenTokens.Padding.Normal,
+                                vertical = DimenTokens.Padding.Normal
+                            ),
+                    )
                 }
-                if(index < items.size -1){
-                    HorizontalDivider(thickness = .5.dp, color = White.copy(alpha = .5f),
-                      //  modifier = Modifier.padding(vertical = DimenTokens.Padding.normal),
+                if (index < items.size - 1) {
+                    HorizontalDivider(
+                        thickness = .5.dp, color = White.copy(alpha = .5f),
+                        //  modifier = Modifier.padding(vertical = DimenTokens.Padding.normal),
                     )
 
                 }
@@ -194,33 +233,42 @@ fun SettingsMenu(modifier: Modifier,items: List<VectorInaTextIcon>,
 }
 
 @Composable
-fun ProfileSummary(modifier: Modifier, initial: String, profileName: String, profileEmail: String){
+fun ProfileSummary(modifier: Modifier, initial: String, profileName: String, profileEmail: String) {
 
     InaCard(modifier = modifier) {
-        ConstraintLayout(modifier = Modifier
-            .padding(DimenTokens.Padding.Normal)) {
+        ConstraintLayout(
+            modifier = Modifier
+                .padding(DimenTokens.Padding.Normal)
+        ) {
             val (profileAvatar, name, email) = createRefs()
 
-            InitialComponent(initial = initial,
-                modifier = Modifier.constrainAs(profileAvatar){
+            InitialComponent(
+                initial = initial,
+                modifier = Modifier.constrainAs(profileAvatar) {
                     start.linkTo(parent.start)
                     top.linkTo(parent.top)
                     bottom.linkTo(parent.bottom)
                 })
-            Text(profileName,
+            Text(
+                profileName,
                 style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.constrainAs(name){
-                    start.linkTo(profileAvatar.end,
-                        margin = DimenTokens.Padding.Small)
+                modifier = Modifier.constrainAs(name) {
+                    start.linkTo(
+                        profileAvatar.end,
+                        margin = DimenTokens.Padding.Small
+                    )
                     top.linkTo(parent.top)
                 })
-            Text(profileEmail,style = MaterialTheme.typography.bodySmall,
-                modifier = Modifier.constrainAs(email){
-                    start.linkTo(profileAvatar.end,
-                        margin = DimenTokens.Padding.Small)
+            Text(
+                profileEmail, style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.constrainAs(email) {
+                    start.linkTo(
+                        profileAvatar.end,
+                        margin = DimenTokens.Padding.Small
+                    )
                     top.linkTo(name.bottom)
                     bottom.linkTo(parent.bottom)
-                } )
+                })
         }
     }
 
@@ -228,6 +276,6 @@ fun ProfileSummary(modifier: Modifier, initial: String, profileName: String, pro
 
 @Preview
 @Composable
-fun PreviewSettingsScreen(){
+fun PreviewSettingsScreen() {
     SettingsScreen()
 }
