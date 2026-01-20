1efb6d4ea58e35531e758080a1aba273e84f15e7
Devmike01 <michaelgbenga91@gmail.com>
2026-01-20 16:15:19 +0100

    updated appliances use case

--- app/src/main/java/dev/gbenga/inaread/domain/usecase/GetAppliancesUseCase.kt
+++ app/src/main/java/dev/gbenga/inaread/domain/usecase/GetAppliancesUseCase.kt
@@ -1,12 +1,15 @@
 package dev.gbenga.inaread.domain.usecase
 
+import dev.gbenga.inaread.data.model.Appliance
 import dev.gbenga.inaread.data.model.ApplianceResponse
+import dev.gbenga.inaread.data.model.toAppliance
 import dev.gbenga.inaread.domain.repository.MetricsRepository
 import javax.inject.Inject
 
 class GetAppliancesUseCase @Inject constructor(val metricsRepository: MetricsRepository) {
 
-    suspend operator fun invoke(): List<ApplianceResponse>{
+    suspend operator fun invoke(): List<Appliance>{
         return metricsRepository.getAppliances()
+            .map { a -> a.toAppliance() }
     }
 }
