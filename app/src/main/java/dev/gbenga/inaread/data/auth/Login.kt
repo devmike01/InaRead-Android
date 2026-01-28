3618d936cd1115da4f70895705a11f018c5db2fb
Devmike01 <michaelgbenga91@gmail.com>
2026-01-28 16:11:41 +0100

    updated login

--- app/src/main/java/dev/gbenga/inaread/data/auth/Login.kt
+++ app/src/main/java/dev/gbenga/inaread/data/auth/Login.kt
@@ -4,12 +4,21 @@ data class UiLogin(val value: String) {
 }
 
 data class LoginResponse(
+    val customerId: String,
+    val meterNo: String,
+    val countryId: String,
+    val meterCategoryId: Int,
+    val createdAt: String,
+    val enabled: Boolean,
     val username: String,
     val email: String,
-    val access : AccessToken
+    val authToken : AccessToken?,
+
 )
 
-data class AccessToken(val access: String, val refresh: String)
+data class AccessToken(val access: String,
+                       val refresh: String,
+                       val expiredIn: Long)
 
 data class LoginRequest(val username: String,
     val password: String)
