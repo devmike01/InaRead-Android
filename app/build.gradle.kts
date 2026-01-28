2ab2dde1c992bd27d31878a01563768c0d8b48ba
Devmike01 <michaelgbenga91@gmail.com>
2026-01-28 16:11:25 +0100

    updated base url

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,12 +34,12 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            buildConfigField("String", "BASE_URL", "\"http://127.0.0.1:3000\"")
+            buildConfigField("String", "BASE_URL", "\"http://localhost:8080/api/v1\"")
         }
 
         debug {
             isMinifyEnabled = false
-            buildConfigField("String", "BASE_URL", "\"http://127.0.0.1:3000\"")
+            buildConfigField("String", "BASE_URL", "\"http://localhost:8080/api/v1\"")
         }
 
     }
