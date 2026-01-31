a0b59a239c9ba62520d204eb556cc7a8432ac2e0
Devmike01 <michaelgbenga91@gmail.com>
2026-01-31 23:45:02 +0100

    deprecate interceptor

--- app/src/main/java/dev/gbenga/inaread/adapters/NetworkResponseInterceptor.kt
+++ app/src/main/java/dev/gbenga/inaread/adapters/NetworkResponseInterceptor.kt
@@ -2,6 +2,7 @@ package dev.gbenga.inaread.adapters
 
 import com.google.gson.Gson
 import dev.gbenga.inaread.data.model.ApiResult
+import dev.gbenga.inaread.tokens.StringTokens
 import okhttp3.Interceptor
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
@@ -9,7 +10,9 @@ import okhttp3.Protocol
 import okhttp3.Response
 import okhttp3.ResponseBody
 import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.HttpException
 
+@Deprecated(replaceWith = ReplaceWith(expression = ""), message = "To be removed. No longer needed")
 class NetworkResponseInterceptor constructor(private val gson: Gson) : Interceptor {
 
     companion object{
@@ -21,15 +24,16 @@ class NetworkResponseInterceptor constructor(private val gson: Gson) : Intercept
         return try {
             chain.proceed(request)
             // TODO: Handle api error
-        }catch (exception: Exception){
-            val body = gson.toJson(ApiResult<Any>(error = "${exception.message}"))
+        }catch (exception: Exception ){
+            // TODO: Log message to firebase crashlytics
+            val body = gson.toJson(ApiResult<Any>(error = StringTokens.UnknownErrorOccured))
             val responseBody: ResponseBody = body.toResponseBody(APPLICATION_JSON)
 
             Response.Builder()
                 .request(request)
                 .protocol(Protocol.HTTP_1_1)
                 .code(500)
-                .message("Internal Server Error")
+                .message(StringTokens.UnknownErrorOccured)
                 .body(responseBody)
                 .build()
         }
