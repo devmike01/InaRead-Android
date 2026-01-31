5196e79cb892f7dee12ae637761506f8d79ed6a1
Devmike01 <michaelgbenga91@gmail.com>
2026-01-31 23:48:03 +0100

    updated api result

--- app/src/main/java/dev/gbenga/inaread/data/model/ApiResult.kt
+++ app/src/main/java/dev/gbenga/inaread/data/model/ApiResult.kt
@@ -1,8 +1,28 @@
 package dev.gbenga.inaread.data.model
 
+import dev.gbenga.inaread.tokens.StringTokens
+
 data class ApiResult<T>(val data: T? = null,
                         val error: String? = null){
 
     val isSuccess = data != null
     val isError = error != null
+
+//    fun successful(onSuccess: (T) -> Unit): ApiResult<T>{
+//        if (data != null && error == null){
+//            onSuccess(data)
+//        }else if ()
+//        return this
+//    }
+//
+//    fun failed(onFailed: (String) -> Unit): ApiResult<T>{
+//        if (error != null && data == null){
+//            onFailed(error)
+//        }else if(error == null){
+//            onFailed(StringTokens.UknownErrorOccured)
+//        }
+//        return this
+//    }
+
+
 }
