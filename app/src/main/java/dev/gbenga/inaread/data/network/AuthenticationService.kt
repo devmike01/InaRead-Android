8ab73ede9e77f0dcb3730fabad644b5f732c6f28
Devmike01 <michaelgbenga91@gmail.com>
2026-01-31 23:48:25 +0100

    updated auth service

--- app/src/main/java/dev/gbenga/inaread/data/network/AuthenticationService.kt
+++ app/src/main/java/dev/gbenga/inaread/data/network/AuthenticationService.kt
@@ -5,14 +5,15 @@ import dev.gbenga.inaread.data.auth.LoginResponse
 import dev.gbenga.inaread.data.auth.SignUpRequest
 import dev.gbenga.inaread.data.auth.SignUpResponse
 import dev.gbenga.inaread.data.model.ApiResult
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthenticationService {
 
     @POST(EndPoints.Login)
-    suspend fun authenticate(loginRequest: LoginRequest): ApiResult<LoginResponse>
+    suspend fun authenticate(@Body loginRequest: LoginRequest): ApiResult<LoginResponse>
 
     @POST(EndPoints.SignUp)
-    suspend fun signUp(request: SignUpRequest): ApiResult<SignUpResponse>
+    suspend fun signUp(@Body request: SignUpRequest): ApiResult<SignUpResponse>
 
 }
