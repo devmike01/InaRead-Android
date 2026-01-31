12897599498f56aa97047a8fd4c1a225154d6bc2
Devmike01 <michaelgbenga91@gmail.com>
2026-01-31 23:47:03 +0100

    added db config

--- app/src/main/java/dev/gbenga/inaread/data/db/InaReadDatabase.kt
+++ app/src/main/java/dev/gbenga/inaread/data/db/InaReadDatabase.kt
@@ -1,7 +1,10 @@
 package dev.gbenga.inaread.data.db
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import dev.gbenga.inaread.data.db.entities.UserEntity
 
+@Database(entities = [UserEntity::class], version = 1, exportSchema = false)
 abstract class InaReadDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
