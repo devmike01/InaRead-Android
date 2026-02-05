53a7bafcb88cdce052e543f8b19ad425ebc55c4c
Devmike01 <michaelgbenga91@gmail.com>
2026-02-05 01:30:24 +0100

    refactored auth apin service

--- app/src/main/java/dev/gbenga/inaread/data/network/AuthenticationService.kt
+++ app/src/main/java/dev/gbenga/inaread/data/network/AuthenticationService.kt
@@ -5,15 +5,18 @@ import dev.gbenga.inaread.data.auth.LoginResponse
 import dev.gbenga.inaread.data.auth.SignUpRequest
 import dev.gbenga.inaread.data.auth.SignUpResponse
 import dev.gbenga.inaread.data.model.ApiResult
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthenticationService {
 
     @POST(EndPoints.Login)
-    suspend fun authenticate(@Body loginRequest: LoginRequest): ApiResult<LoginResponse>
+    suspend fun authenticate(@Body loginRequest: LoginRequest): Response<ApiResult<LoginResponse>>
 
     @POST(EndPoints.SignUp)
-    suspend fun signUp(@Body request: SignUpRequest): ApiResult<SignUpResponse>
+    suspend fun signUp(@Body request: SignUpRequest): Response<ApiResult<SignUpResponse>>
 
 }
