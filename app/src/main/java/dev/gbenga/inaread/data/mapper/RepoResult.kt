2aa378f73da7e7fed44600839c3a717e399af5db
Devmike01 <michaelgbenga91@gmail.com>
2026-02-05 01:29:45 +0100

    updated reporesult

--- app/src/main/java/dev/gbenga/inaread/data/mapper/RepoResult.kt
+++ app/src/main/java/dev/gbenga/inaread/data/mapper/RepoResult.kt
@@ -1,6 +1,24 @@
 package dev.gbenga.inaread.data.mapper
 
+import android.util.Log
+import dev.gbenga.inaread.tokens.StringTokens
+
 sealed interface RepoResult<out T> {
     data class Success<T>(val data: T): RepoResult<T>
     data class Error(val message: String): RepoResult<Nothing>
+
+}
+
+//inline fun <T, R> RepoResult<T>.map(): RepoResult<R>{
+//
+//}
+
+inline fun <T, R> RepoResult<T>.map(
+    onMap: (T) -> R): RepoResult<R>{
+     return when(this){
+        is RepoResult.Error -> {
+            RepoResult.Error(message)
+        }
+        is RepoResult.Success<T> -> RepoResult.Success(onMap(this.data))
+    }
 }
