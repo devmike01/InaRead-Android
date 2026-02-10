bd7d751e7538bf1a7e289079e5bc77e8f437082c
Devmike01 <michaelgbenga91@gmail.com>
2026-02-10 19:36:46 +0100

    added access token

--- app/src/main/java/dev/gbenga/inaread/data/datastore/AccessTokenStore.kt
+++ app/src/main/java/dev/gbenga/inaread/data/datastore/AccessTokenStore.kt
@@ -1,16 +1,23 @@
 package dev.gbenga.inaread.data.datastore
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import androidx.security.crypto.EncryptedSharedPreferences
 import dev.gbenga.inaread.domain.SecureAccessTokenStore
 
-class AccessTokenStore(private val cryptoSharedPrefs: EncryptedSharedPreferences) : SecureAccessTokenStore {
+class AccessTokenStore(private val cryptoSharedPrefs: SharedPreferences) : SecureAccessTokenStore {
 
-    override fun setAccessToken(jwt: String) {
-
+    companion object{
+        private const val ACCESS_TOKEN = "AccessTokenStore.ACCESS_TOKEN";
     }
 
-    override fun getAccessToken(): String {
-        TODO("Not yet implemented")
+    override fun setAccessToken(jwt: String) {
+        cryptoSharedPrefs.edit {
+            putString(ACCESS_TOKEN, jwt)
+        }
+    }
+
+    override fun getAccessToken(): String? {
+        return cryptoSharedPrefs.getString(ACCESS_TOKEN, null)
     }
 }
