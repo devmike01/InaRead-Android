acc1cee9ac97b3cbf12ea8eef5d1af645038fa80
Devmike01 <michaelgbenga91@gmail.com>
2026-02-20 19:25:58 +0100

    fixed userdatastore issue

--- app/src/main/java/dev/gbenga/inaread/data/datastore/UserDataStoreImpl.kt
+++ app/src/main/java/dev/gbenga/inaread/data/datastore/UserDataStoreImpl.kt
@@ -1,5 +1,6 @@
 package dev.gbenga.inaread.data.datastore
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
