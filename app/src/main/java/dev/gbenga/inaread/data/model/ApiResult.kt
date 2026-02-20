6e1728754855ae2c3f95ed91b264c699da02233f
Devmike01 <michaelgbenga91@gmail.com>
2026-02-20 19:25:04 +0100

    updated codes

--- app/src/main/java/dev/gbenga/inaread/data/model/ApiResult.kt
+++ app/src/main/java/dev/gbenga/inaread/data/model/ApiResult.kt
@@ -9,21 +9,4 @@ data class ApiResult<T>(val data: T? = null,
     val isSuccess = data != null
     val isError = error != null
 
-//    fun successful(onSuccess: (T) -> Unit): ApiResult<T>{
-//        if (data != null && error == null){
-//            onSuccess(data)
-//        }else if ()
-//        return this
-//    }
-//
-//    fun failed(onFailed: (String) -> Unit): ApiResult<T>{
-//        if (error != null && data == null){
-//            onFailed(error)
-//        }else if(error == null){
-//            onFailed(StringTokens.UknownErrorOccured)
-//        }
-//        return this
-//    }
-
-
 }
