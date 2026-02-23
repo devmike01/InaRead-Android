f194cb76d641c32394a165c7aeac0f223ed0c9f1
Devmike01 <michaelgbenga91@gmail.com>
2026-02-23 18:13:12 +0100

    added new api

--- app/src/main/java/dev/gbenga/inaread/data/MeterUsageRepositoryImpl.kt
+++ app/src/main/java/dev/gbenga/inaread/data/MeterUsageRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package dev.gbenga.inaread.data
 
 import android.util.Log
+import dev.gbenga.inaread.data.db.PowerUsageSummaryDao
+import dev.gbenga.inaread.data.db.entities.PowerUsageSummaryEntity
 import dev.gbenga.inaread.data.mapper.RepoResult
 import dev.gbenga.inaread.data.model.ConsumptionRecordResponse
 import dev.gbenga.inaread.data.model.MeterResponse
@@ -14,23 +16,66 @@ import dev.gbenga.inaread.domain.repository.MeterUsageRepository
 import dev.gbenga.inaread.domain.services.MeterSummaryApiService
 import dev.gbenga.inaread.utils.UserNotFoundException
 import dev.gbenga.inaread.utils.UserProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.flow.singleOrNull
+import kotlinx.coroutines.withContext
 
 class MeterUsageRepositoryImpl (
     private val meterUsageApiService: MeterUsageStatisticService,
     private val userProvider: UserProvider,
-    private val userDataStore: UserDataStore,
+    private val powerUsageSummaryDao: PowerUsageSummaryDao,
+    private val ioContext: CoroutineDispatcher = Dispatchers.IO
 ) : MeterUsageRepository, NetworkRepository() {
 
 
     override suspend fun getMeterSummaryForDay(
         dateYMD: String
-    ): RepoResult<List<PowerUsageSummaryResponse>> = safeCall {
-        meterUsageApiService
-            .getSummary(userProvider.getCustomerId(),
-                dateYMD)
+    ): RepoResult<List<PowerUsageSummaryResponse>>{
+       return withContext(ioContext){
+           powerUsageSummaryDao
+               .getSummaryByFromDate(dateYMD)
+               ?.let { entity ->
+                   RepoResult.Success(listOf(
+                       PowerUsageSummaryResponse(
+                           periodInDays = entity.periodInDays,
+                           costPerKwh = entity.costPerKwh,
+                           meterType = entity.meterType,
+                           totalMonthPowerUsage = entity.totalMonthPowerUsage,
+                           fromDate = entity.fromDate,
+                           toDate = entity.toDate,
+                           totalSpent = entity.totalSpent
+                       )
+                   ))
+               }
+       } ?: safeCall {
+                meterUsageApiService
+                    .getSummary(dateYMD = dateYMD,
+                        userId = userProvider.getCustomerId())
+            }.apply {
+                if (this is RepoResult.Success){
+                    val resData = data.firstOrNull()
+                    resData?.let {
+                        withContext(ioContext){
+                            powerUsageSummaryDao.insert(PowerUsageSummaryEntity(
+                                fromDate = resData.fromDate,
+                                periodInDays = resData.periodInDays,
+                                costPerKwh = resData.costPerKwh,
+                                meterType = resData.meterType,
+                                totalMonthPowerUsage = resData.totalMonthPowerUsage,
+                                toDate = resData.toDate,
+                                totalSpent = resData.totalSpent,
+
+                            ))
+                        }
+                    }
+
+                }
+            }
+
+
     }
 
     override suspend fun executeAddNewReading(request: PowerUsageRequest)
@@ -46,6 +91,12 @@ class MeterUsageRepositoryImpl (
     override suspend fun executeGetMeterTypes()
     : RepoResult<List<String>> = safeCall {
         meterUsageApiService.getMeterTypes()
+    }
+
+    override suspend fun wipePowerUsageTable() {
+        withContext(ioContext){
+            powerUsageSummaryDao.wipeTable()
+        }
     }
 
 
