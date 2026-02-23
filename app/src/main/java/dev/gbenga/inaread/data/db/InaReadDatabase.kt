999993148e87ecdf98ae72fe35d7b703c398dc9d
Devmike01 <michaelgbenga91@gmail.com>
2026-02-23 18:13:32 +0100

    added new dao to db

--- app/src/main/java/dev/gbenga/inaread/data/db/InaReadDatabase.kt
+++ app/src/main/java/dev/gbenga/inaread/data/db/InaReadDatabase.kt
@@ -2,10 +2,16 @@ package dev.gbenga.inaread.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import dev.gbenga.inaread.data.db.converters.BigDecimalConverter
+import dev.gbenga.inaread.data.db.entities.PowerUsageSummaryEntity
 import dev.gbenga.inaread.data.db.entities.UserEntity
 
-@Database(entities = [UserEntity::class], version = 2, exportSchema = false)
+@Database(entities = [UserEntity::class, PowerUsageSummaryEntity::class], version = 3, exportSchema = false)
+@TypeConverters(BigDecimalConverter::class)
 abstract class InaReadDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
+
+    abstract fun powerUsageSummary(): PowerUsageSummaryDao
 }
