933841c71f524db5939c5cd89f98d834cb290ccb
Devmike01 <michaelgbenga91@gmail.com>
2026-02-27 13:24:08 +0100

    Added new converter to db

--- app/src/main/java/dev/gbenga/inaread/data/db/InaReadDatabase.kt
+++ app/src/main/java/dev/gbenga/inaread/data/db/InaReadDatabase.kt
@@ -4,11 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import dev.gbenga.inaread.data.db.converters.BigDecimalConverter
+import dev.gbenga.inaread.data.db.converters.ListConverter
 import dev.gbenga.inaread.data.db.entities.PowerUsageSummaryEntity
 import dev.gbenga.inaread.data.db.entities.UserEntity
 
 @Database(entities = [UserEntity::class, PowerUsageSummaryEntity::class], version = 4, exportSchema = false)
-@TypeConverters(BigDecimalConverter::class)
+@TypeConverters(BigDecimalConverter::class, ListConverter::class)
 abstract class InaReadDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
