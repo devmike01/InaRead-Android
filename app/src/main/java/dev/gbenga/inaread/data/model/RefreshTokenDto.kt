b465c16a86b9a0d4e3f6643c60cec000c4b04d4a
Devmike01 <michaelgbenga91@gmail.com>
2026-03-03 22:14:58 +0100

    updated refresh token

--- app/src/main/java/dev/gbenga/inaread/data/model/RefreshTokenDto.kt
+++ app/src/main/java/dev/gbenga/inaread/data/model/RefreshTokenDto.kt
@@ -1,11 +1,15 @@
 package dev.gbenga.inaread.data.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class RefreshTokenRequest(
     val token: String,
     val username: String
 )
 
 
+@Serializable
 data class RefreshTokenData(
     val token: String,
     val refreshToken: String,
