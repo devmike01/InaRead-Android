f127d5acecc57a9de8cc52506ca5957b5c8fb9d7
Devmike01 <michaelgbenga91@gmail.com>
2026-03-03 22:13:03 +0100

    udpdated messenger

--- app/src/main/java/dev/gbenga/inaread/data/datastore/Messenger.kt
+++ app/src/main/java/dev/gbenga/inaread/data/datastore/Messenger.kt
@@ -1,13 +1,20 @@
 package dev.gbenga.inaread.data.datastore
 
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 class Messenger(private val dataStore : DataStore<Preferences>) {
 
@@ -19,14 +26,20 @@ class Messenger(private val dataStore : DataStore<Preferences>) {
         dataStore.edit { it[MESSENGER] = message }
     }
 
-    suspend fun clearMessage(){
-        dataStore.edit { it.remove(MESSENGER) }
+    suspend fun clearMessage(io: CoroutineContext = Dispatchers.IO){
+        withContext(io){
+            dataStore.edit { it.remove(MESSENGER) }
+        }
     }
 
-    fun receiveMessage(): Flow<String?>{
+    suspend fun receiveMessage(io: CoroutineContext = Dispatchers.IO): Flow<String?>{
         //  dataStore.edit { it.remove(MESSENGER) }
-        return dataStore.data.map {
-            prefs -> prefs[MESSENGER]
+        return withContext(io){
+            dataStore.data.map {
+                    prefs -> prefs[MESSENGER]
+            }.onCompletion {
+                Log.i("receiveMessage", "completed")
+            }
         }
     }
 
