88212726c3fd94d5af2c41d56ee6c7f7e320182d
Devmike01 <michaelgbenga91@gmail.com>
2026-03-03 22:14:27 +0100

    updated monthly usage

--- app/src/main/java/dev/gbenga/inaread/data/model/MonthlyUsage.kt
+++ app/src/main/java/dev/gbenga/inaread/data/model/MonthlyUsage.kt
@@ -1,15 +1,20 @@
 package dev.gbenga.inaread.data.model
 
+import kotlinx.serialization.Serializable
+import java.math.BigDecimal
+
+@Serializable
 data class MonthlyUsage (
-    val kilowatt: String,
-    val period: String,
-    val cost: String,
-    val day: String,
-    val dayOfMonth: String,
-    val month: String,
-    val fromDate: String,
-    val toDate: String,
-    val meterType: String,
+    val kilowatt: String ="",
+    val period: String="",
+    val cost: String="",
+    val day: String="",
+    val dayOfMonth: String="",
+    val month: String="",
+    val fromDate: String="",
+    val toDate: String="",
+    val meterType: String="",
+  //  val powerConsumption: BigDecimal ="",
 )
 
 
